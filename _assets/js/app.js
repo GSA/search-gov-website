1ab47c137b1a3862604da13183121ac83bc8d621
Amani Farooque <amani.farooque@gmail.com>
2023-02-16 15:47:40 -0500

    Update the highlighting using javascript

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,4 +1,23 @@
 //= require js/uswds.min.js
 
 // Add your custom javascript here
-console.log("Hi from Federalist");
+
+// update sidenav styling (TODO: figure out how to do this without needing JS)
+// find all submenu items that are marked as "current"
+var subNavItems = document.querySelectorAll(".is-current a");
+
+for (i in subNavItems) {
+    // determine the parent folder of the current page URL
+    var subfolder = window.location.href.split('/').slice(0,4).join('/')+"/";
+
+    // add styling if the current page matches a page listed in the submenu
+    if (subNavItems[i].href == window.location.href) {
+        subNavItems[i].classList.add("usa-current");
+    }
+
+    // add styling if the current page belongs to a parent folder in the submenu
+    else if (subNavItems[i].href == subfolder) {
+        subNavItems[i].classList.add("usa-current");
+    }
+   
+}
